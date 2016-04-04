743f33e3e1494b3c39bbe2802ef0f13a521a1fec
Yohan Robert <yohan.robert@runtastic.com>
2016-04-04 22:17:32 +0200

    feat(test): Add test for ResponseModifier.ReadString()

--- response_test.go
+++ response_test.go
@@ -2,7 +2,9 @@ package intercept
 
 import (
 	"github.com/nbio/st"
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -23,4 +25,16 @@ func TestStatus(t *testing.T) {
 	modifier.Status(404)
 	st.Expect(t, resp.StatusCode, 404)
 	st.Expect(t, resp.Status, "404 Not Found")
+}
+
+func TestResponseModifierReadString(t *testing.T) {
+	req := &http.Request{}
+	bodyStr := `{"name":"Rick"}`
+	strReader := strings.NewReader(bodyStr)
+	body := ioutil.NopCloser(strReader)
+	resp := &http.Response{Body: body}
+	modifier := NewResponseModifier(req, resp)
+	str, err := modifier.ReadString()
+	st.Expect(t, err, nil)
+	st.Expect(t, str, bodyStr)
 }

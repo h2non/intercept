8b36d5ba35175cfcd3ec74b6984ad43f5e37cb12
Yohan Robert <yohan.robert@runtastic.com>
2016-04-04 22:08:28 +0200

    feat(test): Add test for NewReponseModifier

--- response_test.go
+++ response_test.go
@@ -1,1 +1,17 @@
 package intercept
+
+import (
+	"github.com/nbio/st"
+	"net/http"
+	"testing"
+)
+
+func TestNewResponseModifier(t *testing.T) {
+	header := http.Header{}
+	req := &http.Request{}
+	resp := &http.Response{Header: header}
+	modifier := NewResponseModifier(req, resp)
+	st.Expect(t, modifier.Request, req)
+	st.Expect(t, modifier.Response, resp)
+	st.Expect(t, modifier.Header, header)
+}

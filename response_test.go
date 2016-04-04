231f1adcbc3f0277ad94ad5389068a64a3c0a997
Yohan Robert <yohan.robert@runtastic.com>
2016-04-04 22:11:47 +0200

    feat(test): Add test for Status

--- response_test.go
+++ response_test.go
@@ -15,3 +15,12 @@ func TestNewResponseModifier(t *testing.T) {
 	st.Expect(t, modifier.Response, resp)
 	st.Expect(t, modifier.Header, header)
 }
+
+func TestStatus(t *testing.T) {
+	req := &http.Request{}
+	resp := &http.Response{}
+	modifier := NewResponseModifier(req, resp)
+	modifier.Status(404)
+	st.Expect(t, resp.StatusCode, 404)
+	st.Expect(t, resp.Status, "404 Not Found")
+}
